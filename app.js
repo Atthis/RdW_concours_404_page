8151425d3551a54fbda747f2df0938487fe43610
Atthis <blanchard.ced@gmail.com>
2023-01-29 20:39:52 +0100

    css work on all page

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ import * as THREE from 'three';
 
 const threeContainer = document.querySelector('#three-container');
 
+const mainContent = document.querySelector('#main-content');
+
+let isOnMain = false;
+
+mainContent.addEventListener('mouseenter', () => {
+  isOnMain = true;
+});
+
+mainContent.addEventListener('mouseleave', () => {
+  isOnMain = false;
+});
+
+
 let mouseX = 0;
 // let mouseY = 0;
 
@@ -37,10 +50,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 threeContainer.appendChild(renderer.domElement);
 
 function render() {
-  camera.position.x += (-mouseX - camera.position.x) * 0.01;
-  // camera.position.y += (-mouseY - camera.position.y) * 0.1;
+  if (isOnMain) {
+    camera.position.x += (-mouseX - camera.position.x) * 0.01;
+    // camera.position.y += (-mouseY - camera.position.y) * 0.1;
 
-  camera.lookAt(scene.position);
+    camera.lookAt(scene.position);
+  }
 
   renderer.render(scene, camera);
 }
